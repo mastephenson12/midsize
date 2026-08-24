9f8eba304cce3436bdfdf31138e9efea9fc48c46
mastephenson12 <masteph123@gmail.com>
2026-08-24 15:27:22 -0700

    Route homeowner domain to the new hub

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -4,10 +4,17 @@ import App from './App.jsx'
 import RoofingApp from './RoofingApp.jsx'
 import MissedLeadCalculator from './MissedLeadCalculator.jsx'
 import SamanthaDemo from './SamanthaDemo.jsx'
+import HomeownerHub from './HomeownerHub.jsx'
 
 const normalizedPath = window.location.pathname.replace(/\/$/, '')
 const isCalculatorPath = normalizedPath === '/missed-lead-calculator'
 const isSamanthaDemoPath = normalizedPath === '/samantha-demo' || normalizedPath === '/samantha.html'
+
+const isHomeownerDomain =
+  window.location.hostname === 'homeowner.midsizeai.com' ||
+  window.location.hostname === 'homeowners.midsizeai.com' ||
+  normalizedPath === '/homeowners' ||
+  new URLSearchParams(window.location.search).get('site') === 'homeowners'
 
 const isRoofingDomain =
   window.location.hostname === 'roofer.midsizeai.com' ||
@@ -16,6 +23,6 @@ const isRoofingDomain =
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {isSamanthaDemoPath ? <SamanthaDemo /> : isCalculatorPath ? <MissedLeadCalculator /> : isRoofingDomain ? <RoofingApp /> : <App />}
+    {isSamanthaDemoPath ? <SamanthaDemo /> : isCalculatorPath ? <MissedLeadCalculator /> : isHomeownerDomain ? <HomeownerHub /> : isRoofingDomain ? <RoofingApp /> : <App />}
   </StrictMode>,
 )
