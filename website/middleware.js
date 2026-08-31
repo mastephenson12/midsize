112dc95e3a706f9c931b0de4bdae71bea3176329
mastephenson12 <masteph123@gmail.com>
2026-08-31 14:50:57 -0700

    Improve homeowner conversion path and SEO

--- website/middleware.js
+++ website/middleware.js
@@ -7,6 +7,19 @@ export const config = {
 export default function middleware(request) {
   const url = new URL(request.url);
   const isRoofingDomain = url.hostname === 'roofers.midsizeai.com';
+  const isHomeownerDomain = url.hostname === 'homeowner.midsizeai.com';
+
+  if (isHomeownerDomain && url.pathname === '/') {
+    return rewrite(new URL('/honest-roofer/', request.url));
+  }
+
+  if (isHomeownerDomain && url.pathname === '/robots.txt') {
+    return rewrite(new URL('/homeowner-robots.txt', request.url));
+  }
+
+  if (isHomeownerDomain && url.pathname === '/sitemap.xml') {
+    return rewrite(new URL('/homeowner-sitemap.xml', request.url));
+  }
 
   if (isRoofingDomain && url.pathname === '/') {
     return rewrite(new URL('/roofers', request.url));
