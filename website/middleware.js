69cc0c2b3699e77c7a7d2c712027834d10e01026
mastephenson12 <masteph123@gmail.com>
2026-08-31 19:14:55 -0700

    Enforce canonical homeowner routes in middleware

--- website/middleware.js
+++ website/middleware.js
@@ -1,13 +1,36 @@
 import { rewrite } from '@vercel/functions';
 
 export const config = {
-  matcher: ['/', '/robots.txt', '/sitemap.xml', '/missed-lead-calculator', '/missed-lead-calculator/'],
+  matcher: [
+    '/',
+    '/robots.txt',
+    '/sitemap.xml',
+    '/honest-roofer',
+    '/honest-roofer/',
+    '/estimate-decoder',
+    '/estimate-decoder/',
+    '/missed-lead-calculator',
+    '/missed-lead-calculator/',
+  ],
 };
 
 export default function middleware(request) {
   const url = new URL(request.url);
   const isRoofingDomain = url.hostname === 'roofers.midsizeai.com';
   const isHomeownerDomain = url.hostname === 'homeowner.midsizeai.com';
+  const isBusinessDomain = url.hostname === 'www.midsizeai.com' || url.hostname === 'midsizeai.com';
+
+  if (isHomeownerDomain && url.pathname.replace(/\/$/, '') === '/honest-roofer') {
+    return Response.redirect(new URL('/', request.url), 308);
+  }
+
+  if (isBusinessDomain && url.pathname.replace(/\/$/, '') === '/honest-roofer') {
+    return Response.redirect('https://homeowner.midsizeai.com/', 308);
+  }
+
+  if (isBusinessDomain && url.pathname.replace(/\/$/, '') === '/estimate-decoder') {
+    return Response.redirect('https://homeowner.midsizeai.com/estimate-decoder/', 308);
+  }
 
   if (isHomeownerDomain && url.pathname === '/') {
     return rewrite(new URL('/honest-roofer/', request.url));
